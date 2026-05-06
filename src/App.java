e7e3f0539998955901a4095b783f8a2b90b018b1
Daniel <duyaguarig@est.ups.edu.ec>
2026-05-06 08:55:53 -0500

    Ordenamiento de nombre y edad

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import Moldels.Person;
+import Controllers.PersonController;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person[] persona = {
+            new Person("Juan", 25),
+            new Person("Ana", 19),
+            new Person("Carlos", 30),
+            new Person("Maria", 22),
+            new Person("Diego", 15),
+            };
+            for(Person person : persona){
+                System.out.println(person);
+            }
+
+            PersonController bsortAge = new PersonController();
+            bsortAge.sortPersonByAge(persona);
+            System.out.println("Ordenados la Edad");
+            for(Person person : persona){
+                System.out.println(person);
+            }
+        }
+    }
+
---
+++ src/Controllers/PersonController.java
@@ -0,0 +1,36 @@
+package Controllers;
+import Moldels.Person;
+public class PersonController {
+    // Metodo que ordena un arreglo de personas 
+    // por su EDAD(age), con metodo insercion
+    public void sortPersonByAge(Person[] personas){
+        for(int i = 1 ; i<personas.length; i++){
+            Person aux = personas[i];
+            int j = i-1;
+            while(j>=0 && personas[j].getAge()>aux.getAge()){
+                personas[j+1] = personas[j];
+                j--;
+
+            }
+            personas[j+1]=aux;
+        }
+
+
+    }
+
+    public void sortPersonByName(Person[] personas){
+        for(int i = 1 ; i<personas.length; i++){
+            Person aux = personas[i];
+            int j = i-1;
+            while(j>=0 && personas[j].getName().compareTo(aux.getName())){
+                personas[j+1] = personas[j];
+                j--;
+
+            }
+            personas[j+1]=aux;
+        }
+
+
+    }
+}
+
